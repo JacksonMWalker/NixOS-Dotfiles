113c45fbc0956b60ec6b775f091cdbce774da7d5
JacksonMWalker <jacksonmwalker@proton.me>
2026-07-09 14:02:04 +0000

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    function fish_greeting
+        #
+    end
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/sponk2/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# Created by `pipx` on 2024-05-03 03:54:04
+set PATH $PATH /home/sponk2/.local/bin
+
+# Java_home
+set -x JAVA_HOME /usr/lib/jvm/java-21-openjdk/
+
+# Encore
+# set encore $PATH /home/sponk2/.encore/bin/encore
+set ENCORE_INSTALL /home/sponk2/.encore/bin/encore $PATH
+
+# Starship auto start
+starship init fish | source
+
+fish_add_path /home/sponk2/.spicetify
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# Hints
+set -U fish_autosuggestion_enabled 1
+set -g fish_color_autosuggestion 586e75 # Blanco amarillento (máximo contraste)
+
+
+alias ff="fastfetch -l small"
+alias nixcfg="sudo nvim  /etc/nixos/configuration.nix"
+alias nixup="sudo nixos-rebuild switch --flake /etc/nixos#default"
